4db77492f30924880fdfe18d733c8e22d64804a9
Fresh <fr3shnuts@gmail.com>
2023-01-29 17:03:19 -0800

    what.. fix

---
+++ HTB/what_does_the_f_say/input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int userInput;
+
+  scanf("%d", &userInput);
+  printf("%d", userInput);
+
+
+  return 0;
+}
---
+++ HTB/what_does_the_f_say/sys_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(){
+
+  system("/bin/sh");
+
+  return 0;
+
+}
